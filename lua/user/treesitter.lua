530d6ce304ee639374a84d6a943dc29b976fc4eb
Duy Pham <duypham9895@gmail.com>
2022-12-06 20:19:41 +0700

    Confit Treesitter for MacOS

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,17 +1,43 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-  ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
+  ensure_installed = { "graphql", "html", "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css",
+    "rust", "java", "yaml", "markdown", "markdown_inline" }, -- one of "all" or a list of languages
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    -- additional_vim_regex_highlighting = true,
+    -- disable = { "css" }, -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {
+    --   "#cc241d",
+    --   "#a89984",
+    --   "#b16286",
+    --   "#d79921",
+    --   "#689d6a",
+    --   "#d65d0e",
+    --   "#458588",
+    -- },
+    -- termcolors = {
+    --   "Red",
+    --   "Green",
+    --   "Yellow",
+    --   "Blue",
+    --   "Magenta",
+    --   "Cyan",
+    --   "White",
+    -- },
+  }
 })
