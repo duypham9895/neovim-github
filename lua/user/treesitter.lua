b2e1ec85f60bd78d62c36727fcef41493d059c1f
Duy Pham <duypham9895@gmail.com>
2022-12-07 10:32:13 +0700

    Updated sync for MacOS

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -9,10 +9,13 @@ configs.setup({
   ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    -- additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false,
     -- disable = { "css" }, -- list of language that will be disabled
   },
   autopairs = {
+    enable = true,
+  },
+  autotag = {
     enable = true,
   },
   indent = { enable = true },
@@ -21,23 +24,24 @@ configs.setup({
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {
-    --   "#cc241d",
-    --   "#a89984",
-    --   "#b16286",
-    --   "#d79921",
-    --   "#689d6a",
-    --   "#d65d0e",
-    --   "#458588",
-    -- },
-    -- termcolors = {
-    --   "Red",
-    --   "Green",
-    --   "Yellow",
-    --   "Blue",
-    --   "Magenta",
-    --   "Cyan",
-    --   "White",
-    -- },
+    colors = {
+      "#cc241d",
+      "#a89984",
+      "#b16286",
+      "#d79921",
+      "#689d6a",
+      "#d65d0e",
+      "#458588",
+    },
+    termcolors = {
+      "Red",
+      "Green",
+      "Yellow",
+      "Blue",
+      "Magenta",
+      "Cyan",
+      "White",
+    },
+
   }
 })
